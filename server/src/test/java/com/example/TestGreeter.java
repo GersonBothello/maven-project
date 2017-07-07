472d63d4439fa1a81f0edc6d6de10eec19bed591
Gerson Bothello <gerson.Bothello@Ceeonsoft.com>
2017-07-06 23:04:47 -0700

    test

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -24,6 +24,7 @@ public class TestGreeter {
     assertThat(greeter.greet(someone), containsString(someone));
   }
 
+  
   @Test
   public void greetShouldIncludeGreetingPhrase() {
     String someone = "World";
